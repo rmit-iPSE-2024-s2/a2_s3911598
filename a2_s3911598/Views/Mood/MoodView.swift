b9b6472f51b86fb1cfe7e407a72ba2be0664e62d
Lea Wang <s3911598@student.rmit.edu.au>
2024-10-13 21:43:13 +1100

    Update MoodView.swift

--- a2_s3911598/Views/Mood/MoodView.swift
+++ a2_s3911598/Views/Mood/MoodView.swift
@@ -32,7 +32,7 @@ struct MoodView: View {
     
     @State private var heartBeat = false
     
-    @State private var username: String = UserDefaults.standard.string(forKey: "username") ?? "Default User"
+    @State private var username: String = UserDefaults.standard.string(forKey: "username") ?? ""
     
     /// The user's profile, containing basic user information.
     var userProfile: Profile
@@ -63,123 +63,85 @@ struct MoodView: View {
 
     var body: some View {
         let moodData = injectedMoodData ?? moodDataFromContext
-        ScrollView {
-            VStack(alignment: .leading, spacing: 20) {
-                Text("Moods")
-                    .font(.custom("Chalkboard SE", size: 24))
-                    .padding([.leading, .top], 16)
-
-                // Dynamically display the username
-                Text("Welcome, \(username.isEmpty ? userProfile.name : username)")
-                    .font(.custom("Chalkboard SE", size: 16))
-                    .bold()
-                    .padding([.leading], 16)
+        VStack {
+            Text("Moods")
+                .font(.custom("Chalkboard SE", size: 24))
+                .padding([.leading], 16)
+                .padding([.top], 0)
+                .frame(maxWidth: .infinity, alignment: .leading)
+            
+            ScrollView {
+                VStack(alignment: .leading, spacing: 20) {
+                    // Dynamically display the username
+                    Text("Welcome, \(username.isEmpty ? userProfile.name : username)")
+                        .font(.custom("Chalkboard SE", size: 16))
+                        .bold()
+                        .padding([.leading], 16)
+                    
+                    // Displays a random motivational quote
+                    VStack(alignment: .leading) {
+                        if let quote = quoteModel.result {
+                            Text("\"\(quote.q)\"")
+                                .font(.custom("Chalkboard SE", size: 16))
+                                .multilineTextAlignment(.leading)
+                                .onTapGesture {
+                                    quoteModel.fetchQuote()
+                                }
+                        }
+                    }
+                    .padding([.leading, .trailing])
                     .padding([.top], -10)
-                
-                // Displays a random motivational quote
-                VStack(alignment: .leading) {
-                    if let quote = quoteModel.result {
-                        Text("\"\(quote.q)\"")
-                            .font(.custom("Chalkboard SE", size: 16))
-                            .multilineTextAlignment(.leading)
-                            .onTapGesture {
-                                quoteModel.fetchQuote()
-                            }
-                    }
-                }
-                .padding([.leading, .trailing])
-                .padding([.top], -10)
-                .frame(minHeight: 40, maxHeight: 90, alignment: .top)
-
-                // Recent mood record section
-                cardView {
-                    VStack(alignment: .leading, spacing: 10) {
-                        HStack {
-                            Image(systemName: "heart.fill")
-                                .resizable()  // Make the image resizable
-                                .frame(width: 30, height: 30) 
-                                .foregroundColor(.red)
-                                .scaleEffect(heartBeat ? 1.2 : 1.0)  // Set the scaling effect for heartbeat animation
-                                .animation(.easeInOut(duration: 0.8).repeatForever(autoreverses: true), value: heartBeat)  // Loop the heartbeat animation
-                                .onAppear {
-                                    heartBeat = true  // Start the animation when the view appears
-                                }
-
-                            Text("My daily mood record")
-                                .font(Font.custom("Chalkboard SE", size: 20))
-                            Spacer()
-                            Button(action: {
-                                showCalendar = true
-                            }) {
-                                HStack {
-                                    Image(systemName: "eyes")
-                                    Text("View all")
-                                        .font(Font.custom("Chalkboard SE", size: 16))
-                                }
-                            }
-                            .sheet(isPresented: $showCalendar) {
-                                CalendarView()
-                            }
-                        }
-                        .padding(.horizontal)
-                    }
-                    .padding(.vertical)
-                }
-
-                // "How do you feel today" card with mood tracking
-                cardView {
-                    VStack(alignment: .leading, spacing: 10) {
-                        HStack {
-                            Text("How do you feel today?")
-                                .font(Font.custom("Chalkboard SE", size: 26))
-                            Spacer()
-                        }
-                        .padding(.horizontal)
-                        
-                        Button(action: {
-                            showMoodTracking = true
-                        }) {
-                            Text("Go record")
-                                .font(Font.custom("Chalkboard SE", size: 18))
-                                .frame(maxWidth: .infinity)
-                                .padding()
-                                .background(Color("primaryMauve"))
-                                .foregroundColor(.white)
-                                .cornerRadius(10)
-                                .padding(.horizontal)
-                        }
-                        .padding(.top, 16)
-                        .sheet(isPresented: $showMoodTracking) {
-                            MoodTrackingView(isActive: $showMoodTracking)
-                        }
-                    }
-                    .padding(.vertical, 16)
-                }
-
-                // Display today's mood record
-                if let todayMood = moodForToday(moodData: moodData) {
+                    .frame(minHeight: 40, maxHeight: 90, alignment: .top)
+                    
+                    // Recent mood record section
                     cardView {
                         VStack(alignment: .leading, spacing: 10) {
-                            Text("Today's Mood Record")
-                                .font(Font.custom("Chalkboard SE", size: 20))
-                            
-                            Text("Time: \(timeFormatter.string(from: todayMood.date))")
-                                .font(Font.custom("Chalkboard SE", size: 18))
-                            
-                            Text("Mood: \(todayMood.moodLevel)")
-                                .font(Font.custom("Chalkboard SE", size: 18))
-
-                            
-                            if !todayMood.notes.isEmpty {
-                                Text("Notes: \(todayMood.notes)")
-                                    .font(Font.custom("Chalkboard SE", size: 18))
-                                    .padding(.top, 4)
+                            HStack {
+                                Image(systemName: "heart.fill")
+                                    .resizable()  // Make the image resizable
+                                    .frame(width: 30, height: 30)
+                                    .foregroundColor(.red)
+                                    .scaleEffect(heartBeat ? 1.2 : 1.0)  // Set the scaling effect for heartbeat animation
+                                    .animation(.easeInOut(duration: 0.8).repeatForever(autoreverses: true), value: heartBeat)  // Loop the heartbeat animation
+                                    .onAppear {
+                                        heartBeat = true  // Start the animation when the view appears
+                                    }
+                                
+                                Text("My daily mood record")
+                                    .font(Font.custom("Chalkboard SE", size: 20))
+                                Spacer()
+                                Button(action: {
+                                    showCalendar = true
+                                }) {
+                                    HStack {
+                                        Image(systemName: "eyes")
+                                        Text("View all")
+                                            .font(Font.custom("Chalkboard SE", size: 16))
+                                    }
+                                }
+                                .sheet(isPresented: $showCalendar) {
+                                    CalendarView()
+                                }
                             }
+                            .padding(.horizontal)
+                        }
+                        .padding(.vertical)
+                    }
+                    
+                    // "How do you feel today" card with mood tracking
+                    cardView {
+                        VStack(alignment: .leading, spacing: 10) {
+                            HStack {
+                                Text("How do you feel today?")
+                                    .font(Font.custom("Chalkboard SE", size: 26))
+                                Spacer()
+                            }
+                            .padding(.horizontal)
                             
                             Button(action: {
-                                showFriendsPicker = true
+                                showMoodTracking = true
                             }) {
-                                Text("Share this with...")
+                                Text("Go record")
                                     .font(Font.custom("Chalkboard SE", size: 18))
                                     .frame(maxWidth: .infinity)
                                     .padding()
@@ -189,30 +151,70 @@ struct MoodView: View {
                                     .padding(.horizontal)
                             }
                             .padding(.top, 16)
+                            .sheet(isPresented: $showMoodTracking) {
+                                MoodTrackingView(isActive: $showMoodTracking)
+                            }
                         }
-                        .padding()
+                        .padding(.vertical, 16)
                     }
-                    .padding(.bottom)
-                } else {
-                    // If no mood record for today, show a placeholder message
-                    Text("No mood recorded for today yet.")
-                        .font(Font.custom("Chalkboard SE", size: 18))
-                        .foregroundColor(.gray)
-                        .padding()
-                }
-
-                Spacer()
-            }
-        }
-        .sheet(isPresented: $showFriendsPicker) {
-            FriendsPickerView(friends: friends, showsharemessgae: true,selectedFriends: $selectedFriends, isPresented: $showFriendsPicker)
-        }
-        .background(Color("AppBackground").edgesIgnoringSafeArea(.all))
-        .onAppear {
-            quoteModel.fetchQuote()
-            if let savedUsername = UserDefaults.standard.string(forKey: "username") {
-                            username = savedUsername
+                    
+                    // Display today's mood record
+                    if let todayMood = moodForToday(moodData: moodData) {
+                        cardView {
+                            VStack(alignment: .leading, spacing: 10) {
+                                Text("Today's Mood Record")
+                                    .font(Font.custom("Chalkboard SE", size: 20))
+                                
+                                Text("Time: \(timeFormatter.string(from: todayMood.date))")
+                                    .font(Font.custom("Chalkboard SE", size: 18))
+                                
+                                Text("Mood: \(todayMood.moodLevel)")
+                                    .font(Font.custom("Chalkboard SE", size: 18))
+                                
+                                
+                                if !todayMood.notes.isEmpty {
+                                    Text("Notes: \(todayMood.notes)")
+                                        .font(Font.custom("Chalkboard SE", size: 18))
+                                        .padding(.top, 4)
+                                }
+                                
+                                Button(action: {
+                                    showFriendsPicker = true
+                                }) {
+                                    Text("Share this with...")
+                                        .font(Font.custom("Chalkboard SE", size: 18))
+                                        .frame(maxWidth: .infinity)
+                                        .padding()
+                                        .background(Color("primaryMauve"))
+                                        .foregroundColor(.white)
+                                        .cornerRadius(10)
+                                        .padding(.horizontal)
+                                }
+                                .padding(.top, 16)
+                            }
+                            .padding()
                         }
+                        .padding(.bottom)
+                    } else {
+                        // If no mood record for today, show a placeholder message
+                        Text("No mood recorded for today yet.")
+                            .font(Font.custom("Chalkboard SE", size: 18))
+                            .foregroundColor(.gray)
+                            .padding()
+                    }
+                    
+                    Spacer()
+                }
+            }
+            .sheet(isPresented: $showFriendsPicker) {
+                FriendsPickerView(friends: friends, showsharemessgae: true,selectedFriends: $selectedFriends, isPresented: $showFriendsPicker)
+            }
+            .onAppear {
+                quoteModel.fetchQuote()
+                if let savedUsername = UserDefaults.standard.string(forKey: "username") {
+                    username = savedUsername
+                }
+            }
         }
     }
 
